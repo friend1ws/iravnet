62e2c5f583405d801ef85e1bbc30d69e3ee4a448
friend1ws <friend1ws@gmail.com>
2019-04-23 06:07:14 +0900

    add mapping quality filter and gnomad filtering

--- filter_irav.py
+++ filter_irav.py
@@ -5,8 +5,9 @@ import pysam
 
 input_file = sys.argv[1]
 
-# exac_db = pysam.Tabixfile("gnomad.exomes.r2.1.1.sites.vcf.bgz")
-exac_db = pysam.Tabixfile("https://storage.googleapis.com/gnomad-public/release/2.1.1/vcf/genomes/gnomad.genomes.r2.1.1.sites.vcf.bgz")
+gnomad_exome_db = pysam.Tabixfile("gnomad.exomes.r2.1.1.sites.vcf.bgz")
+gnomad_genome_db = pysam.Tabixfile("gnomad.genomes.r2.1.1.sites.vcf.bgz")
+# exac_db = pysam.Tabixfile("https://storage.googleapis.com/gnomad-public/release/2.1.1/vcf/genomes/gnomad.genomes.r2.1.1.sites.vcf.bgz")
 
 with open(input_file, 'r') as hin:
     header2ind = {}
@@ -14,7 +15,7 @@ with open(input_file, 'r') as hin:
     for i, cname in enumerate(header):
         header2ind[cname] = i
 
-    print '\t'.join(header) + '\t' + "ExAC"
+    print '\t'.join(header) + '\t' + "gnomAD_exome" + '\t' + "gnomAD_genome"
 
     for line in hin:
         F = line.rstrip('\n').split('\t')
@@ -33,8 +34,8 @@ with open(input_file, 'r') as hin:
         ref_mut = F[header2ind["Ref_Mut"]]
         alt_mut = F[header2ind["Alt_Mut"]] 
 
-        AF = 0
-        for record_line in exac_db.fetch(chr_mut, start_mut - 3, end_mut + 3):
+        AF_exome = 0
+        for record_line in gnomad_exome_db.fetch(chr_mut, start_mut - 3, end_mut + 3):
             record = record_line.split('\t')
             if record[0] != chr_mut: continue
             if record[1] != str(start_mut): continue
@@ -44,8 +45,21 @@ with open(input_file, 'r') as hin:
             infos = record[7].split(';')
             for info in infos:
                 if info.startswith("AF="):
-                    AF = float(info.replace("AF=", ''))
-                                           
-        print '\t'.join(F) + '\t' + str(round(AF, 4))
+                    AF_exome = float(info.replace("AF=", ''))
+                            
+                       
+        AF_genome = 0
+        for record_line in gnomad_genome_db.fetch(chr_mut, start_mut - 3, end_mut + 3):
+            record = record_line.split('\t')
+            if record[0] != chr_mut: continue
+            if record[1] != str(start_mut): continue
+            if record[3] != ref_mut: continue
+            if record[4] != alt_mut: continue
+            
+            infos = record[7].split(';')
+            for info in infos:
+                if info.startswith("AF="):
+                    AF_genome = float(info.replace("AF=", ''))
 
+        print '\t'.join(F) + '\t' + str(round(AF_exome, 4)) + '\t' + str(round(AF_genome, 4))
 
--- irav_detect.sh
+++ irav_detect.sh
@@ -21,7 +21,7 @@ OUTPUT=$2
 
 REF=/home/w3varann/database/GRCh37/GRCh37.fa
 
-samtools mpileup ${INPUT} -l boundary_proc.bed -f ${REF} -O > ${OUTPUT}.tmp1.txt
+samtools mpileup ${INPUT} -l boundary_proc.bed -f ${REF} -O -q 20 > ${OUTPUT}.tmp1.txt
 
 python proc_variant.py ${OUTPUT}.tmp1.txt > ${OUTPUT}.tmp2.txt
 
