af837bb59427e39f06aa554396358ed560fc7f21
friend1ws <friend1ws@gmail.com>
2019-09-01 23:37:06 +0900

    modify make_filtered_taret.sh

--- prep_data/db/make_filterd_target.sh
+++ prep_data/db/make_filterd_target.sh
@@ -3,13 +3,14 @@
 # hg19 grc
 annot_utils boundary boundary.bed.gz --donor_size 3,6 --acceptor_size 6,1 --grc
 
-annot_utils exon exon.bed.gz --grc
+# annot_utils exon exon.bed.gz --grc
 
 zcat TCGA_742_8.bed.gz | awk '$1 ~ /^[0-9XY]+$/ {OFS="\t"; print $1, $2-10, $2+10}' > control_8_broden.bed
 
-bedtools subtract -a boundary.bed.gz -b control_8_broden.bed -A > boundary_proc.tmp.bed
+# bedtools subtract -a boundary.bed.gz -b control_8_broden.bed -A > boundary_proc.tmp.bed
 
-bedtools subtract -a boundary_proc.tmp.bed -b exon.bed.gz -A -f 0.9 | \
+# bedtools subtract -a boundary_proc.tmp.bed -b exon.bed.gz -A -f 0.9 | \
+bedtools subtract -a boundary.bed.gz -b control_8_broden.bed -A | \
     bedtools merge -i - | \
     awk '$1 ~ /^[0-9XY]+$/ {OFS="\t"; print $1, $2-10, $3}' > \
     boundary_filtered.hg19.grc.bed
