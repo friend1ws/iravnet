d58b6f074761f2eb9e387c8a2b9874dc299a603b
friend1ws <friend1ws@gmail.com>
2019-10-09 21:47:38 +0000

    add hg38 target region file

--- prep_data/db/make_filterd_target.sh
+++ prep_data/db/make_filterd_target.sh
@@ -15,6 +15,18 @@ bedtools subtract -a boundary.bed.gz -b control_8_broden.bed -A | \
     awk '$1 ~ /^[0-9XY]+$/ {OFS="\t"; print $1, $2-10, $3}' > \
     boundary_filtered.hg19.grc.bed
 
+# hg19
+cat boundary_filtered.hg19.grc.bed | awk -F'\t' 'BEGIN { OFS = "\t" } {print "chr"$1, $2, $3}' > boundary_filtered.hg19.bed
+
+# hg38
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz
+
+liftOver boundary_filtered.hg19.bed hg19ToHg38.over.chain.gz boundary_filtered.hg38.tmp.bed unmapped.txt
+
+awk 'BEGIN {OFS="\t"} {if ($1 !~ /_/) print}' boundary_filtered.hg38.tmp.bed | sort -k1,1 -k2,2n -k3,3n > boundary_filtered.hg38.bed
+
+awk 'BEGIN {OFS="\t"} {print substr($1, 4), $2, $3}' boundary_filtered.hg38.bed | sort -k1,1 -k2,2n -k3,3n > boundary_filtered.hg38.grc.bed
+
 rm -rf boundary.bed.gz*
 rm -rf exon.bed.gz*
 rm -rf control_8_broden.bed
